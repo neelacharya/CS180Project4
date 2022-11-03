ac5ed7822a8f635f387145873d2660e0d3ab83da
neelacharya <63558977+neelacharya@users.noreply.github.com>
2022-11-03 00:27:34 -0400

    Add files via upload

---
+++ Buyable.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public interface Buyable {
+    public String getName();
+
+    public void setName(String name);
+
+    public ArrayList<String> getStore();
+
+    public void setStore(ArrayList<String> store);
+
+    public void addStore(String store);
+
+    public String getDescription();
+
+    public void setDescription(String description);
+
+    public int getQuantity();
+
+    public void setQuantity(int quantity);
+
+    public double getPrice();
+
+    public void setPrice(double price);
+}
+
