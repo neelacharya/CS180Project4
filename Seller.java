0809368ea9ba6a01cb0461eab4e9f7d2e8a847f4
YadavSuhani <112599773+YadavSuhani@users.noreply.github.com>
2022-11-04 15:59:06 -0400

    Add files via upload

---
+++ Seller.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Seller {
+    private ArrayList<Store> stores;
+    private String name;
+
+    public Seller(String name, ArrayList<Store> stores) {
+        this.name = name;
+        this.stores = stores;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Store> getStores() {
+        return stores;
+    }
+
+    public void setStores(ArrayList<Store> stores) {
+        this.stores = stores;
+    }
+
+    public void createProduct(Store store, Shoe shoe) {
+        /*
+        checks if store exists in the seller's list of stores;
+        if it does - adds shoe to the store
+        if not, error message is printed.
+         */
+    }
+
+    public void removeProduct(Store store, Shoe shoe) {
+        /*
+        checks if store exists in the seller's list of store,
+        if it does, shoe is removed from that store
+        if not, error message is printed.
+         */
+    }
+
+    public void editProduct(Store store, Shoe shoe, int choice) {
+        /*
+        checks if seller has this store
+        checks if given shoe exists in that store
+        checks if choice is correct
+        sets the new value accordingly
+        error messages are printed accordingly
+         */
+    }
+
+    public void viewStoreInfo(Store store) {
+        /*
+
+         */
+    }
+}
