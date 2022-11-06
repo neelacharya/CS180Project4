078462004e445dd2767a2f5b5dbc517915356355
YadavSuhani <112599773+YadavSuhani@users.noreply.github.com>
2022-11-06 17:26:23 -0500

    Update Seller.java
    implemented all core functionalities

--- Seller.java
+++ Seller.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Seller {
@@ -26,35 +28,54 @@ public class Seller {
     }
 
     public void createProduct(Store store, Shoe shoe) {
-        /*
-        checks if store exists in the seller's list of stores;
-        if it does - adds shoe to the store
-        if not, error message is printed.
-         */
+        if (stores.contains(store)) {
+            store.addProduct(shoe);
+        } else {
+            System.out.println("Store does not exist");
+        }
     }
 
     public void removeProduct(Store store, Shoe shoe) {
-        /*
-        checks if store exists in the seller's list of store,
-        if it does, shoe is removed from that store
-        if not, error message is printed.
-         */
+        if (stores.contains(store)) {
+            store.removeProduct(shoe);
+        } else {
+            System.out.println("Store does not exist");
+        }
     }
 
-    public void editProduct(Store store, Shoe shoe, int choice) {
-        /*
-        checks if seller has this store
-        checks if given shoe exists in that store
-        checks if choice is correct
-        sets the new value accordingly
-        error messages are printed accordingly
-         */
+    public void editProduct(Store store, Shoe shoe, Shoe newShoe) {
+        if (stores.contains(store)) {
+            if (store.getProducts().contains(shoe)) {
+                store.editProduct(shoe, newShoe);
+            } else {
+                System.out.println("No such shoe exists");
+            }
+        } else {
+            System.out.println("No such shoe exists");
+        }
     }
 
-    public void viewStoreInfo(Store store) {
-        /*
-        calls returnPurchase from store to log seller's 
-        selling details.
-         */
+    public void viewStoreInfo() {
+        ArrayList<String> sales;
+        try {
+            PrintWriter printWriter = new PrintWriter(new FileWriter(name + ".txt"));
+            for (Store store: stores) {
+                printWriter.println("--------------------");
+                printWriter.println(store.getName());
+                sales = store.getSales();
+                if (sales.size() == 0) {
+                    printWriter.println("No sales yet.");
+                } else {
+                    for (String sale: sales) {
+                        printWriter.println(sale);
+                    }
+                }
+            }
+            printWriter.flush();
+            printWriter.close();
+        } catch (Exception ex) {
+            System.out.println("Error in displaying file");
+            return;
+        }
     }
 }
